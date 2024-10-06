980c24ac4b7f8fd459fe1a9f04b4d0376fbf6699
roblox-ye <3108792043@qq.com>
2024-10-06 16:59:27 +0800

    Update ROBLOX-CNVIP-XIAOYE.lua

--- ROBLOX-CNVIP-XIAOYE.lua
+++ ROBLOX-CNVIP-XIAOYE.lua
@@ -3,15 +3,14 @@ local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
     Title = "确认身份",
     Text = "正在验证....",
-    Duration = 3, 
-    wait(3)  
+    Duration = 5, 
+    wait（2）
 })
 
 local StarterGui = game:GetService("StarterGui")
 local Players = game:GetService("Players")
 local WhitelistedPlayers = {
-["DFTRQ24"] = true,
-    
+    ["DFTRQ24"] = true,
 }
 
 local function IsWhitelisted(player)
@@ -25,9 +24,10 @@ local isLocalPlayerWhitelisted = IsWhitelisted(localPlayer)
 if isLocalPlayerWhitelisted then
     StarterGui:SetCore("SendNotification", {
         Title = "白名单认证",
-        Text = "玩家:"..localPlayer.Name.."，检测完毕尊贵的白名单玩家，稍后会加载脚本",
+        Text = "玩家:"..localPlayer.Name.."，检测完毕白名单玩家，稍后会加载脚本",
         Duration = 7,
     })
      loadstring(game:HttpGet("https://raw.githubusercontent.com/roblox-ye/QQ515966991/refs/heads/main/XIAOYE-YE.lua"))()
-    localPlayer:Kick("你没有被加入白名单，请加qq3108792043获得")
+else
+    localPlayer:Kick("你没有被加入白名单，请加QQ群3108792043获得")
 end
