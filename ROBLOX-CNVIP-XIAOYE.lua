f8d94ae66237741d1b71e9b2978f3256662001b3
roblox-ye <3108792043@qq.com>
2024-10-11 15:43:05 +0800

    Update ROBLOX-CNVIP-XIAOYE.lua

--- ROBLOX-CNVIP-XIAOYE.lua
+++ ROBLOX-CNVIP-XIAOYE.lua
@@ -24,10 +24,10 @@ local isLocalPlayerWhitelisted = IsWhitelisted(localPlayer)
 if isLocalPlayerWhitelisted then
     StarterGui:SetCore("SendNotification", {
         Title = "白名单认证",
-        Text = "玩家:"..localPlayer.Name.."，检测完毕白名单玩家，稍后会加载脚本",
+        Text = "玩家:"..localPlayer.Name.."，检测完毕尊贵的白名单玩家，稍后会加载脚本",
         Duration = 7,
     })
      loadstring(game:HttpGet("https://raw.githubusercontent.com/roblox-ye/QQ515966991/refs/heads/main/ROBLOX-XIAOYE-www..lua"))()
 else
-    localPlayer:Kick("你没有被加入白名单，请到QQ群515966991获得")
+    localPlayer:Kick("你没有被加入白名单，白名单5元，请加QQ3108792043购买")
 end
